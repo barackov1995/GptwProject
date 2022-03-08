ae5de642384fa31c40a2c34fd9a5ac14081fee05
unknown <dejan.barackov@greatplacetowork.com>
2022-03-08 16:46:09 +0100

    create password page

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 - I used Webdriver.io framework with Cucumber. 
 - My task was to write automated tests for Create Password page 
 
-- Test release script: npx wdio run wdio.conf.js
-
+## Resources
 - I used following resources of the libraries:
 1. WebdriverIO (https://webdriver.io/)
 2. Cucumber (https://cucumber.io/)
+
+## How to run my test: 
+- Use this script to run test: npx wdio run wdio.conf.js
--- features/pageobjects/page.js
+++ features/pageobjects/page.js
@@ -6,8 +6,10 @@ module.exports = class Page {
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
+    * this part is implemented this way because I don't have a part of the test before this one that leads to an email with a link to this page. 
     */
+   
     open() {
-        return browser.url(`https://dev-login.greatplacetowork.com/identity/Account/PasswordSetup?userId=028c1f89-9a14-4a07-b1a7-99188c361915&code=CfDJ8OIqJbWxBhFHnng258tZB4LVOnQEdu%2f%2b7wJ2Q4IL3Q0mjlgzcFpuRiCcidrEAVz7zrcZsvDAD5cAH52CWUbyAGFo1pw0UC4TqzofKh7deBtEHN7aErMgEBDkZZeXYZriTuFGAyBkqT4C1658PCcFYlgPsa6wQmNPbAN0P5ZddoSzLb6BzsxyMWYXbZBRZZohFfKlX5DdzmwJZCQF2RQfkUqckX0%2fo%2bYkBNlZcihz3Z0Z&culture=en-US`)
+        return browser.url(`https://qa-login.greatplacetowork.com/identity/Account/PasswordSetup?userId=a8a5ff9f-c62e-4621-9517-cba1424aa034&code=CfDJ8FieISvxm%2b5Brn%2f2YhAG2jDDU%2fYX%2fbQmGKP76fc%2febVOoVBfDzS1IK%2feJXHEoOQwZf%2b9FctLSYl6XN7ORV0%2fYL0PWJDuHKPwwQ8BzfeshnH1UXTGvQ7z4uXcO5R0cRUvZfvqxGZz3QIkJaPu2k3%2fadXALv05qA599o1k6LRTvwpKk5O9mNL37bDpbp9jzTmjVMKcNpQs3gqqHLlIFVOMSI2aR9ibWfWsEXaXDYZ4F%2bdI&culture=en-US`)
     }
 }
